4c092ee85192155161f67feeb6bb07146910a4f4
plutko <pawel.pluta@gmail.com>
2017-12-19 01:13:01 +0100

    started from scratch from a SDL tutorial. Created Game loop that create window, renderer and update function

---
+++ BuilderDash/Game.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <SDL.h>
+#include <iostream>
+
+class Game
+{	
+public:
+	Game();
+	~Game();
+		
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running();
+	
+private:
+	int counter = 0; // test counter
+	bool isRunning;
+	SDL_Window *window;
+	SDL_Renderer * renderer;
+};
